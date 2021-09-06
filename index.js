47f0f7809df8c02b602837f64bde26e4a37aa9da
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-06 16:28:51 +0530

    Create user route

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-
+const userRoute = require("./routes/user");
 
 // Mongo database connection
 dotenv.config();
-mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
-    console.log("Connected to Database!!")
-})
+mongoose.connect(
+  process.env.MONGODB_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to Database!!");
+  }
+);
 
 // middleware
 app.use(express.json());
 app.use(helmet());
-app.use(morgan("common"))
+app.use(morgan("common"));
 
-
-
+app.use("/api/user", userRoute);
 
 app.listen(8800, () => {
-    console.log("Backend server is running!!!")
-})
+  console.log("Backend server is running!!!");
+});
