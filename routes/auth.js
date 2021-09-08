06e51122180fca34b0480bb0bbda8b8536bffac9
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-08 09:33:53 +0530

    Create login

--- routes/auth.js
+++ routes/auth.js
@@ -23,4 +23,22 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("wrong password");
+
+    res.status(200).json("login successfully");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
