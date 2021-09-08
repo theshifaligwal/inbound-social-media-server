cd03bd1b857d14c058d80a428b5f36ddcee07e46
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-08 17:09:35 +0530

    Get a post

--- routes/posts.js
+++ routes/posts.js
@@ -63,6 +63,18 @@ router.put("/:id/like", async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+  //get a post
+
+router.get("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
   
   
 module.exports = router;
