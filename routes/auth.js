079a61e304dd6177aad6b97d217d4014fe48ec59
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-08 09:03:35 +0530

    Bcrypt password

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,21 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 router.post("/register", async (req, res) => {
-  const newUser = new User({
-    userName: req.body.userName,
-    email: req.body.email,
-    password: req.body.password,
-  });
-
   try {
+    // genrate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // genrate new user
+    const newUser = new User({
+      userName: req.body.userName,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    // save user and return respond
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
